566616506ea47274b3f6949c0d98bf9237081aba
sadaf mahmodui <iamsadafmhmdi@gmail.com>
2020-10-20 14:37:25 +0330

    implement dialog

---
+++ src/components/ِDialog/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Button from '../Button/index';
+import './index.css'
+
+
+function Dialog({header, description, action}) {
+    return(
+        <div className='container flex-display-center'>
+            <div className='dialog'>
+                <h2 id='header'>
+                    {header}
+                </h2>
+                <div id='description'>
+                    {description}
+                </div>
+                <div>
+                    {action}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Dialog;
