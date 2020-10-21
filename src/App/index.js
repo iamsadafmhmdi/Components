e3a2b4624ee9d2f0046f2b80ea2593571aca9d96
sadaf mahmodui <iamsadafmhmdi@gmail.com>
2020-10-21 14:31:39 +0330

    fixed equalSign space

--- src/App/index.js
+++ src/App/index.js
@@ -4,28 +4,28 @@ import Dialog from '../components/ِDialog/index';
 
 
 function App() {
-    const [showDialog, setShowDialog]= useState(false);
-    const handleClickButton= useCallback(() => {
+    const [showDialog, setShowDialog] = useState(false);
+    const handleClickButton = useCallback(() => {
         setShowDialog(true);
     }, []);
 
     return(
         <div>
             <Button  
-            onClick= {handleClickButton}>
+            onClick = {handleClickButton}>
                 click
             </Button>
             {
                 showDialog && <Dialog
-                header= {'this is header'}
-                description= {'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quasi deserunt vitae suscipit assumenda qui nobis corrupti magni culpa. Obcaecati voluptatem exercitati onem consectetur deserunt ullam officia adip isci cumque sequi accusamus.'}
-                action= {
+                header = {'this is header'}
+                description = {'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quasi deserunt vitae suscipit assumenda qui nobis corrupti magni culpa. Obcaecati voluptatem exercitati onem consectetur deserunt ullam officia adip isci cumque sequi accusamus.'}
+                action = {
                     <div>
                         <Button>
                             Confirm
                         </Button>
                         <Button
-                        disabled= {'disabled'}>
+                        disabled = {'disabled'}>
                             Cancel
                         </Button>
                     </div>
