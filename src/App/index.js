205008e89cc63e34034755a89aeab8042b314166
iamsadafmhmdi <66523851+iamsadafmhmdi@users.noreply.github.com>
2020-10-29 13:58:04 +0330

    added danger button (#4)
    * added danger button
    
    * fixed an issue
    
    * fix danger prop
    
    * fix an issue
    
    * fix a minor issue
    
    * add empty space
    
    * add ternary operator
    
    * add empty line
    
    * fix using danger and disabled at same time
    
    * fix a minor issue
    
    * some improvements on css, some lint and syntax improvement in App/index.js, replace npm with yarn
    
    * fix space and one class
    
    * fix misspelled
    
    Co-authored-by: Mehdi Babapour <mehdi.babapour@zardalu.dev>

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,6 @@
 import React, {useState, useCallback} from 'react';
-import Button from '../components/Button/index';
-import Dialog from '../components/ِDialog/index';
+import Button from '../components/Button';
+import Dialog from '../components/ِDialog';
 
 
 function App() {
@@ -11,26 +11,17 @@ function App() {
 
     return(
         <div>
-            <Button  
-            onClick = {handleClickButton}>
-                click
-            </Button>
-            {
-                showDialog && <Dialog
-                header = {'this is header'}
-                description = {'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quasi deserunt vitae suscipit assumenda qui nobis corrupti magni culpa. Obcaecati voluptatem exercitati onem consectetur deserunt ullam officia adip isci cumque sequi accusamus.'}
-                action = {
+            <Button onClick={handleClickButton}>Click</Button>
+            {showDialog && <Dialog
+                header={'this is header'}
+                description={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quasi deserunt vitae \
+                suscipit assumenda qui nobis corrupti magni culpa. Obcaecati voluptatem exercitati onem consectetur deserunt ullam officia adip isci cumque sequi accusamus.`}
+                action={
                     <div>
-                        <Button>
-                            Confirm
-                        </Button>
-                        <Button
-                        disabled = {'disabled'}>
-                            Cancel
-                        </Button>
+                        <Button>Confirm</Button>
+                        <Button disabled={true} danger={true}>Cancel</Button>
                     </div>
-                }/>
-            }
+                }/>}
         </div>
     );
 };
