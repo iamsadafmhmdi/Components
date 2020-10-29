bb2b862b7130e917c5649522d5af9fa1e18e8eed
sadaf mahmodui <iamsadafmhmdi@gmail.com>
2020-10-29 15:48:06 +0330

    fix multi line props

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './index.css';
 
 
-function Checkbox({children,checked,onChange}) {
+function Checkbox({
+    children,
+    checked,
+    onChange
+}) {
     return(
             <label className='label'>
                     <input className='checkbox' type='checkbox' checked={checked} onChange={onChange} />
