997e4cf579ee5834854f8cf4d6958190c5c7b8a9
sadaf mahmodui <iamsadafmhmdi@gmail.com>
2020-10-29 15:36:52 +0330

    edit multi line props

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './index.css';
 
 
-function TextInput({value,onChange,placeholder, ...props}) {
+function TextInput({
+    value,
+    onChange,
+    placeholder,
+    ...props
+}) {
     return(
             <input type='text' value={value} onChange={onChange} placeholder={placeholder} {...props}/>
     );
