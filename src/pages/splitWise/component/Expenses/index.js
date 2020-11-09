b26a29667bc6b654654f05e1e3a72dda0e22b4c5
sadaf mahmodui <iamsadafmhmdi@gmail.com>
2020-11-10 00:04:30 +0330

    insert delete button

--- src/pages/splitWise/component/Expenses/index.js
+++ src/pages/splitWise/component/Expenses/index.js
@@ -2,18 +2,24 @@ import React from "react";
 import {Button} from "../../../../components";
 
 
-function Expenses({data}) {
+function Expenses({data, deleteButton}) {
     return (
-        <div className='expenses-container'>
+        <div>
             <div>
                 {data.map((expense) => (
-                    <p key={expense.id} className='expenses'>
-                        <span className='subject'>{expense.subject.toUpperCase()}</span>
+                    <p key={expense.id}>
+                        <span>{expense.subject.toUpperCase()}</span>
                         {expense.costs.map((person) => (
                             <span key={person.id}>
                                 &nbsp;&nbsp; - {person.name}:{person.portion}
                             </span>
                         ))}
+                        &nbsp;&nbsp;
+                        <Button
+                            children={"Delete"}
+                            danger={true}
+                            onClick={() => deleteButton(expense.id)}
+                        />
                     </p>
                 ))}
             </div>
